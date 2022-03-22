42a571e064db563bfb89eab60ad36ae6427485fb
Olga Antushevich <antushevich@gmail.com>
2022-03-22 11:00:59 +0200

    init: add main files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// меню Бургер -------------------------------------------------------------------------
+
+// const iconMenu = document.querySelector(".menu__icon");
+// const bodyMenu = document.querySelector(".menu__body");
+// if (iconMenu) {
+//     iconMenu.addEventListener("click", function (event) {
+
+//         document.body.classList.toggle("_lock"); //! добавляем класс "_lock" в SCSS для запрета прокрутки при активном меню-бургере
+//         iconMenu.classList.toggle("_active");
+//         bodyMenu.classList.toggle("_active");
+
+//     });
+// };
+
+// убираю placeholder при фокусе -----------------------------------------------------------
+
+// const form = document.forms[0];
+// const input = form.elements.inputEmail;
+// const inputPlaceholder = input.placeholder;
+
+// input.addEventListener("focus", function (event) {
+
+//     input.placeholder = "";
+// });
+
+// input.addEventListener("blur", function (event) {
+
+//     input.placeholder = inputPlaceholder;
+// });
+
+
+//! ============================================================================================
+
+
