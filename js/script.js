25ca4d2a949fcb83fc14231a4f37bed7aaf2953a
Olga Antushevich <antushevich@gmail.com>
2022-03-24 18:40:45 +0200

    feat: add functions for work with api

--- js/script.js
+++ js/script.js
@@ -38,3 +38,55 @@ const choices = new Choices(elementSelect, {
 	searchEnabled: false,
 	itemSelectText: '',
 });
+
+//! ======= Work with api ===================================================================
+
+const API_KEY = '738879d919fa4fac96337701bee3640e';
+const newsList = document.querySelector('.news__list');
+
+const getData = async (url) => {
+	const response = await fetch(url, {
+		headers: {
+			'X-Api-Key': API_KEY,
+		}
+	});
+
+	const data = await response.json();
+
+	return data;
+};
+
+const renderCard = (data) => {
+	newsList.textContent = '';
+	data.forEach((news) => {
+		const card = document.createElement('li');
+		card.classList.add('news__item', 'card');
+
+		card.innerHTML = `
+		<img class="card__image" src="${news.urlToImage}"
+							alt="${news.title}" width="270" height="200">
+						<div class="card__text">
+							<h3 class="card__title">
+								<a class="card__link" href="${news.url}" target="_blank">${news.title}</a>
+							</h3>
+							<p class="card__description">${news.description}</p>
+							<div class="card__footer footer-card">
+								<time class="footer-card__datetime" datetime="${news.publishedAt}">
+									<span>${news.publishedAt}</span><span> 11:06</span>
+								</time>
+								<div class="footer-card__author">${news.author}</div>
+							</div>
+						</div>
+		`;
+
+		newsList.append(card);
+	});
+};
+
+const loadNews = async () => {
+	const data = await getData('https://newsapi.org/v2/top-headlines?country=ua');
+	renderCard(data.articles);
+};
+
+loadNews();
+
